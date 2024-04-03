1252a6f28c3a5f8b8ae0ef0bfd68ab4e9ec7b228
William Lempinen <wlempin@gmail.com>
2024-04-03 10:51:50 +0300

    before challenges, chapter 6

--- src/main/java/org/craftinginterpreters/lox/Lox.java
+++ src/main/java/org/craftinginterpreters/lox/Lox.java
@@ -52,20 +52,35 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-        for (Token token : tokens) {
-            System.out.println(token);
+        // Stop if there was a syntax error.
+        if (hadError) {
+            return;
         }
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
 
-    static void error(int line, String message) {
+    //error is invoked in the Scanner class
+    public static void error(int line, String message) {
         report(line, "", message);
     }
 
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    //invoked in Parser class
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 
 
